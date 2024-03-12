d5570621aba6aa8e17dc5c243a034b0ad26601df
Ray <rlawlsdn9583@gmail.com>
2024-03-13 02:13:03 +0900

    [백준]1316_S5_B_https://www.acmicpc.net/problem/1316

--- BaekJoon/1316_그룹단어체커.py
+++ BaekJoon/1316_그룹단어체커.py
@@ -16,3 +16,19 @@ for word in data:
         group_num += 1
 
 print(group_num)
+
+
+## 괜찮은 풀이
+# N = int(input())
+# cnt = N
+
+# for i in range(N):
+#     word = input()
+#     for j in range(0, len(word)-1):
+#         if word[j] == word[j+1]:
+#             pass
+#         elif word[j] in word[j+1:]:
+#             cnt -= 1
+#             break
+
+# print(cnt)
