030e90e5e535e1478d53683ad3541cec8f4af5f5
Ray <rlawlsdn9583@gmail.com>
2024-03-18 02:39:57 +0900

    [백준]1018_S4_C_https://www.acmicpc.net/problem/1018

--- BaekJoon/1018_체스판다시칠하기.py
+++ BaekJoon/1018_체스판다시칠하기.py
@@ -2,11 +2,11 @@ row, column = map(int, input().split())
 board = [list(input()) for _ in range(row)]
 
 start = ['BW', 'WB']
-b_case, w_case = [[''.join([s[0] if (i+j) % 2 == 0 else s[1] for j in range(column)]) for i in range(row)] for s in start]  # 체스판 생성 (시작이 B 이거나 W)
+b_case, w_case = [[''.join([s[0] if (i+j) % 2 == 0 else s[1] for j in range(column)]) for i in range(row)] for s in start]  # 기본 체스판 생성 (시작이 B 이거나 W)
 b_count, w_count = [[[0 for _ in range(column)] for _ in range(row)] for _ in range(2)]
-b_sum, w_sum = [[[0 for _ in range(column)] for _ in range(row)] for _ in range(2)]
 min_case = row * column
 
+# 입력 체스판이랑 기본 체스판을 비교하여 바꿔야 될 영역 가중치 부여
 for i in range(row):
     for j in range(column):
         if board[i][j] != b_case[i][j]:
@@ -14,7 +14,7 @@ for i in range(row):
         elif board[i][j] != w_case[i][j]:
             w_count[i][j] = 1
     
-
+# 8x8 영역 체스판으로 쪼개어 가중치 영역의 합 중 최소를 저장
 for a in range(row-7):
     for b in range(column-7):
         b_temp = 0
