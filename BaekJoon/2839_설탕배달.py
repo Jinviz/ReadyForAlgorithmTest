cacef77f4220484f26abf9f44691367b3844de27
Ray <rlawlsdn9583@gmail.com>
2024-03-22 01:15:45 +0900

    [백준]2839_S4_A_https://www.acmicpc.net/problem/2839

--- BaekJoon/2839_설탕배달.py
+++ BaekJoon/2839_설탕배달.py
@@ -1,3 +1,4 @@
+## 부루트 포스 풀이
 sugar = int(input())
 div, remain = divmod(sugar, 5) 
 result = 0
@@ -17,28 +18,30 @@ while True:
 
 print(int(result))
 
-div = math.floor(sugar / 5)
-remain = sugar % 5
+## 모든 케이스를 생각하여 풀이
 
-if div > 0:
-    if remain == 0: 
-        result = div   
-    elif remain == 1: 
-        if sugar >= 6:
-            result = div - 1 + 2
-        else:
-            result = -1
-    elif remain == 2:
-        if sugar >= 12:
-            result = div - 2 + 4 
-        else:
-            result = -1
-    elif remain == 3: 
-        result = div + 1
-    else:
-        result = div - 1 + 3
-else:
-    if remain == 3:
-        result = 1
-    else:
-        result = -1
+# div = math.floor(sugar / 5)
+# remain = sugar % 5
+
+# if div > 0:
+#     if remain == 0: 
+#         result = div   
+#     elif remain == 1: 
+#         if sugar >= 6:
+#             result = div - 1 + 2
+#         else:
+#             result = -1
+#     elif remain == 2:
+#         if sugar >= 12:
+#             result = div - 2 + 4 
+#         else:
+#             result = -1
+#     elif remain == 3: 
+#         result = div + 1
+#     else:
+#         result = div - 1 + 3
+# else:
+#     if remain == 3:
+#         result = 1
+#     else:
+#         result = -1
