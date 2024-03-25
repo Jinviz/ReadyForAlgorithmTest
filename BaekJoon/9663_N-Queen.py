0ec12c55e5eb977e9b0280d1cd6118f843067df2
JinwooKim <rlawlsdn9583@gmail.com>
2024-03-25 22:56:02 +0900

    flag

--- BaekJoon/9663_N-Queen.py
+++ BaekJoon/9663_N-Queen.py
@@ -2,54 +2,40 @@
 from itertools import combinations
 
 N = int(input())
-visit = [[0 for _ in range(N)] for _ in range(N)]# 중복과 순서없이 체스 N개가 놓일 수 있는 체스판 영역 선택
-count = 0
-direction = [-1,0,1]
+visit = [[0 for _ in range(N)] for _ in range(N)]   # 중복과 순서없이 체스 N개가 놓일 수 있는 체스판 영역 선택
 case = []
-def backtracking(current, cnt, visit):
+def backtracking(current, cnt, visited):
     if cnt == N:
-        return visited
+        if visited not in case:
+            case.append(visited)
+        return 
     # 해당 보드영역 방문 처리
-    a, b = divmod(current, N)
-    visited = visit
-    visited[a][b] = 1
+    a, b = current
+    board = removeArea(a, b, visited)
     
-    for dx in direction:
-        for dy in direction:
-            if 0 <= a+dx < N and 0 <= b+dy < N: # 보드판 벗어나기 방지
-                if dx != 0 and dy != 0: # 제자리 머물기 제외
-                    if visited[a+dx][b+dy] == 0:    # 방문한 영역 제외
-                        backtracking(N*(a+dx)+b+dy, cnt+1, visited)
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] != 1:
+                backtracking((i,j), cnt+1, [row[:] for row in board])
 
-for n in range(N):
-    case.append(backtracking(n,0,visit))
+# 퀸의 이동반경을 케이스에서 제외시키기
+def removeArea(x, y, board):
+    for i in range(N):
+        board[i][y] = 1
+        board[x][i] = 1
+        if 0 <= x-i < N and 0 <= y-i < N:
+            board[x-i][y-i] = 1
+        if 0 <= x-i < N and 0 <= y+i < N:
+            board[x-i][y+i] = 1
+        if 0 <= x+i < N and 0 <= y-i < N:
+            board[x+i][y-i] = 1
+        if 0 <= x+i < N and 0 <= y+i < N:
+            board[x+i][y+i] = 1
 
-result = set(case)
-print(len(result))
+        return board
+    
+for n in range(N*N):
+    a, b = divmod(n, N)
+    backtracking((a,b),0,visit)
 
-
-# board = [[0] *N] *N 
-
-# # 퀸의 활동 영역 1로 처리
-# board[num/N][num%N] = 1
-
-
-# 조합 경우의 수
-
-
-
-
-# # 칠해야할 영역에 1이 있는 지 확인
-# for i in range(N):
-#     # i가 범위를 넘지않는 경우를 구해야 함
-
-#     # 대각선 방향 체크
-#     if board[num/N -i][num%N -i] == 1 or board[num/N -i][num%N +i] == 1 or board[num/N +i][num%N -i] == 1 or board[num/N +i][num%N +i] == 1:
-#         break
-#     # 일자 방향 체크
-#     elif board[num/N -i][num%N] == 1 or board[num/N + i][num%N] == 1 or board[num/N][num%N -i] == 1 or board[num/N][num%N +i] == 1:
-#         break
-
-
-#     # 대각선 방향 
-#     board[num/N -i][num%N -i], board[num/N -i][num%N +i], board[num/N +i][num%N -i], board[num/N +i][num%N +i] = 1, 1, 1, 1
+print(len(case))
