1714e805368bfe299ab5a024bb9843186d75b1d2
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-04-08 11:51:14 +0900

    savepoint

--- BaekJoon/1260_DFS와BFS.py
+++ BaekJoon/1260_DFS와BFS.py
@@ -1,15 +1,31 @@
-# N = int(input())
-# node_list = [list(map(int, input().split())) for _ in range(N)]
+from collections import deque
 
-# def dfs(g, v, visited):
-#     visited[start] = True
-#     print(v, end=' ')
-#     for node in g[v]:
-#         if visited[node] == False:
-#             dfs(g, node, visited)
+info = list(map(int, input().split()))
+graph = [[] for _ in range(info[0]+1)]
+link_info = [list(map(int, input().split())) for _ in range(info[1])]
 
-# visited = [False for _ in range(N)]
-# dfs(node_list, 1, visited)
+for l in link_info:
+    graph[l[0]].append(l[1])
 
+bfs_visited, dfs_visited = [False, False for _ in range(info[0])] * 2
+bfs_result, dfs_result = [] * 2
 
-from collections import deque/
+# dfs
+def dfs(g, start, visited):
+    visited[start] = True
+    bfs_result.append(start)
+
+    for node  in g[start]: 
+        if visited[node] == False:
+            bfs(g, node, visited)
+
+# bfs
+def bfs(g, start, visited):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        bfs_result.append(v)
+
+# 낮은 순서대로 접근은 오름차순 정렬 내장함수 사용해야 함
