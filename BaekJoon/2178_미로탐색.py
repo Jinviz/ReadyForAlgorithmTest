9797a3c70dd489285bb3c15d47d439c9c862b930
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-04-11 11:25:47 +0900

    [백준]2178_미로탐색_S1_https://www.acmicpc.net/problem/2178

--- BaekJoon/2178_미로탐색.py
+++ BaekJoon/2178_미로탐색.py
@@ -1,29 +1,22 @@
 from collections import deque
-dxdy = [(1,0),(0,1),(-1,0),(0,-1)]
-# dx = [-1, 1, 0, 0]
-# dy = [0, 0 ,-1, 1]
 
 N, M = map(int, input().split())
 maze = [list(map(int, input())) for _ in range(N)]
+
+count = [[1 for _ in range(M)] for _ in range(N)]
 visited = [[False for _ in range(M)] for _ in range(N)]
+dxdy = [(0,1),(1,0),(-1,0),(0,-1)]
+queue = deque([(0,0)])
 
-def dfs(n, m, visited, cnt):
-    visited[n][m] = True
-    cnt += 1
-    print(cnt)
-
-    if n == N-1 and m == M-1:
-        return True
-
+while queue:
+    x, y = queue.popleft()
     for d in dxdy:
-        nx, ny = n + d[0], m + d[1]
-        if n+d[0] < N and m+d[1] < M:
+        nx, ny = x + d[0], y + d[1]
+        if 0 <= nx < N and 0 <= ny < M:
             if maze[nx][ny] == 1 and visited[nx][ny] == False:
-                result = dfs(nx, ny, visited, cnt)
-                if result == True:
-                    return
-    
-    return cnt
+                queue.append((nx,ny))
+                visited[nx][ny] = True
+                count[nx][ny] = count[x][y] + 1
 
-count = dfs(0, 0, visited, 0)
-print(count)
+print(count[N-1][M-1])
+
