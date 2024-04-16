22963eec6fbb70cc96e94f4ca4a67cb51e5f0508
JinwooKim <rlawlsdn9583@gmail.com>
2024-04-16 12:42:56 +0900

    savepoint

--- BaekJoon/1012_유기농배추.py
+++ BaekJoon/1012_유기농배추.py
@@ -1,9 +1,10 @@
-testCase = int(input())
+case = int(input())
 M, N, position = [], [], []
-for _ in range(testCase):
+for _ in range(case):
     m, n, k = map(int, input().split())
     posit = [tuple(map(int, input().split())) for _ in range(k)]
     M.append(m)
     N.append(N)
     position.append(posit)
+
 
