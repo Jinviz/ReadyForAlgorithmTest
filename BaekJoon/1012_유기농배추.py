e4ca3f53bf06283f4513696d32efc8c818224841
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-04-16 18:55:35 +0900

    savepoint

--- BaekJoon/1012_유기농배추.py
+++ BaekJoon/1012_유기농배추.py
@@ -10,7 +10,7 @@ for _ in range(case):
     m.append(a)
     n.append(b)
     k.append(c)
-    cabbage = [tuple(map(int, input().split())) for _ in range(k)]
+    cabbage = [tuple(map(int, input().split())) for _ in range(c)]
     location.append(cabbage)
 
 # 케이스별 M x N 땅(ground) 빈 리스트 생성 
@@ -24,15 +24,28 @@ for c in range(case):
     for point in location[c]:
         ground[c][point[1]][point[0]] = 1 
 
-            
 
+# 방문 리스트 생성 / 길이: 튜플로 이루어진 리스트의 길이
+visited = []
+for n in range(case):
+    visit = [0 for _ in range(len(location[n]))]
+    visited.append(visit)
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-
-
+# DFS 검사
 for state in range(case):
-    for 
-    queue = deque([(0,0)])
+    cabbageList = location[state]
+    queue = deque([location[state][0]])    # location[n] = (x,y)
+
     while queue:
+        node = queue.popleft()
+        for n in range(4):
+            nx = node[0] + dx[n]
+            ny = node[1] + dy[n]
+            if tuple((nx,ny)) in cabbageList:
+              # 이러면 발견한 튜플의 위치를 몰라서 안될 듯
+
+
+        
