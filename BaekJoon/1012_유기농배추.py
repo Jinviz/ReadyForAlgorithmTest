4c0dc50e4e46869540df66bd6b207087d431c657
JinwooKim <rlawlsdn9583@gmail.com>
2024-04-17 21:18:55 +0900

    [백준]1012_S2_C_https://www.acmicpc.net/problem/1012

--- BaekJoon/1012_유기농배추.py
+++ BaekJoon/1012_유기농배추.py
@@ -1,17 +1,16 @@
 from collections import deque
 
-# M[n]: 가로 / N[n]: 세로 / location[n]: (m,n)위치 정보 리스트
+# M[n]: 가로 / N[n]: 세로 / total_location[n]: (m,n)위치 정보 리스트
 case = int(input())
-location= []    # 케이스별 벌레 위치값(튜플)로 이루어진 리스트 저장
-m, n, k = [], [], []
+total_location= []    # 케이스별 벌레 위치값(튜플)로 이루어진 리스트 저장
+m, n = [], []
 
 for _ in range(case):
     a, b, c = map(int, input().split())
     m.append(a)
     n.append(b)
-    k.append(c)
     cabbage = [tuple(map(int, input().split())) for _ in range(c)]
-    location.append(cabbage)
+    total_location.append(cabbage)
 
 # 케이스별 M x N 땅(total_ground) 빈 리스트 생성 
 total_ground = []
@@ -21,27 +20,33 @@ for c in range(case):
 
 # 빈 total_ground에 배추 위치 표시
 for c in range(case):
-    for point in location[c]:
+    for point in total_location[c]:
         total_ground[c][point[1]][point[0]] = 1 
-
-
-# 방문 리스트 생성 / 길이: 튜플로 이루어진 리스트의 길이
-visited = []
-for n in range(case):
-    visit = [0 for _ in range(len(location[n]))]
-    visited.append(visit)
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
+total_count = [0 for _ in range(case)]
 
 # DFS 검사
 for state in range(case):
-    cabbage = location[state][0]
     ground = total_ground[state]
-    queue = deque([ground[]])    # cabbageList[0] = (x,y) 튜플타입
+    x = m[state]
+    y= n[state]
 
-    while queue:
-        for n in range(4):
-
-
+    for b, row in enumerate(ground):
+        for a, element in enumerate(row):
+            if element == 1:
+                queue = deque([(a,b)])
+                while queue:
+                    start = queue.popleft()
+                    for d in range(4):
+                        nx = start[0] + dx[d]
+                        ny = start[1] + dy[d]
+                        if 0 <= nx < x and 0 <= ny < y and ground[ny][nx] == 1:
+                            ground[ny][nx] = 0  # 방문 처리
+                            queue.append((nx,ny))
+                total_count[state] += 1
         
+
+for n in range(case):
+    print(total_count[n])
