78f1c43463368b3acfb62f631d210b61e797b58d
JinwooKim <rlawlsdn9583@gmail.com>
2024-04-24 18:19:48 +0900

    [백준]13305_S3_B_https://www.acmicpc.net/problem/13305

--- BaekJoon/13305_주유소.py
+++ BaekJoon/13305_주유소.py
@@ -5,9 +5,7 @@ oil_price.pop()
 # 첫 출발은 무조건 주유를 하긴 해야 함
 # n번 주유소가 가장 싸면 이후 남은 거리 모두 주유(마지막 도시 제외)
 # n번 주유소에서 남은 주유소 가격 중 n번보다 싼 도시까지 거리를 주유
-print(km, oil_price)
 sum_price = 0
-print(oil_price.index(min(oil_price)))
 
 for n in range(N-1):
     if oil_price[n] == min(oil_price):
@@ -16,8 +14,11 @@ for n in range(N-1):
         break
 
     else:
-        for i in range(n, oil_price.index(min(oil_price))):
-            sum_price += km[i] * oil_price[n]
-            km[i] = 0
+        for until in range(n+1, N-1):
+            if oil_price[until] < oil_price[n]:
+                for i in range(n, until):
+                    sum_price += km[i] * oil_price[n]
+                    km[i] = 0
+                break
 
 print(sum_price)
