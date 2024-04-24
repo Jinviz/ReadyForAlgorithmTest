f8444e58540dacdabf07fdfd37b85605a2b42379
JinwooKim <rlawlsdn9583@gmail.com>
2024-04-24 18:48:49 +0900

    savepoint

---
+++ BaekJoon/10610_30.py
@@ -0,0 +1,15 @@
+N = input()
+num = []
+for n in N:
+    num.append(int(n))
+num.sort(reverse=True)
+current = 0
+
+if 0 in num:
+    print(-1)
+else:
+    for 
+    current = int("".join(num))
+    if int("".join(num)) % 30 == 0:
+        print(current)
+        break
