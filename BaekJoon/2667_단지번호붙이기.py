366a97698034bf7047b0f92a295370268ce36ab4
JinwooKim <rlawlsdn9583@gmail.com>
2024-04-30 18:07:37 +0900

    [백준]2667_S1_A_https://www.acmicpc.net/status?user_id=kb1812&problem_id=2667&from_mine=1

--- BaekJoon/2667_단지번호붙이기.py
+++ BaekJoon/2667_단지번호붙이기.py
@@ -1,7 +1,6 @@
 from collections import deque
 N = int(input())
 ground = [list(map(int, input())) for _ in range(N)]
-# visited = [[False for _ in range(N)] for _ in range(N) ]
 town = 0    # 단지 수
 town_house = [] #단지별 숫자
 dx = [1,-1,0,0]
@@ -15,18 +14,18 @@ for n in range(N):
     for m in range(N):
         if ground[n][m] == 1:
             queue = deque([(n,m)])
+            ground[n][m] = 0
             town += 1                   # 단지 숫자 추가
-            town_house.append(0)        # 단지의 집 개수에 대한 정보
+            town_house.append(1)        # 단지의 집 개수에 대한 정보
             while queue:
                 x, y = queue.popleft()
-                town_house[-1] += 1     # 단지 내의 집 개수 추가 
-                ground[x][y] = 0        # 방문 처리
                 for i in range(4):
                     nx, ny = x+dx[i], y+dy[i]
-                    if ground[nx][ny] == 1:
+                    if 0 <= nx < N and 0 <= ny < N and ground[nx][ny] == 1:
                         queue.append((nx,ny))
-
+                        ground[nx][ny] = 0      # 방문 처리
+                        town_house[-1] += 1     # 단지 내의 집 개수 추가 
 
 print(town)
-print(town_house)
-
+for house in sorted(town_house):
+    print(house)
