a31a9883efbae41ad547930217ac0f63e1fe6e8f
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-05-16 16:59:07 +0900

    savepoint

--- BaekJoon/15649_N과M.py
+++ BaekJoon/15649_N과M.py
@@ -1,23 +1,43 @@
 N, M = map(int, input().split())
 number = [str(n) for n in range(1,N+1)]
-print(number)
-case = list()
 
-def progression(first, num):
+def progression(case, num):
     if len(case) == M:
         result = " ".join(case)
         print(result)
         case.pop()
+        return
     
-    else:
-        for n in num:
-            case.append(n)
-            print(case)
-            num.remove(n)
-            progression(False, num)
+    for n in num:
+        print(n)
+        case.append(n)
+        num_list = num[:]
+        num_list.remove(n)
+        progression(case, num_list)
+        print("end")
 
-progression(True, number)
+progression([], number)
 
-    
+# def dfs():
+#     if len(s) == m:
+#         print(' '.join(map(str, s)))
+#         return
+#     for i in range(1, n+1):
+#         if visited[i]:
+#             continue
+#         visited[i] = True
+#         s.append(i)
+#         dfs()
+#         s.pop()
+#         # print(s)
+#         # print(visited)
+#         visited[i] = False
+            
+
+# n, m = map(int, input().split())
+# s = []
+# visited = [False] * (n+1)
+
+# dfs()
         
     
