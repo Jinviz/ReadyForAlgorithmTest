801b412c73e5d2dd47a9ded55db2ec8c3312330d
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-05-16 19:50:20 +0900

    [백준]15649_S3_C_https://www.acmicpc.net/problem/15649

--- BaekJoon/15649_N과M.py
+++ BaekJoon/15649_N과M.py
@@ -5,39 +5,13 @@ def progression(case, num):
     if len(case) == M:
         result = " ".join(case)
         print(result)
-        case.pop()
         return
     
     for n in num:
-        print(n)
         case.append(n)
         num_list = num[:]
         num_list.remove(n)
         progression(case, num_list)
-        print("end")
+        case.pop()
 
 progression([], number)
-
-# def dfs():
-#     if len(s) == m:
-#         print(' '.join(map(str, s)))
-#         return
-#     for i in range(1, n+1):
-#         if visited[i]:
-#             continue
-#         visited[i] = True
-#         s.append(i)
-#         dfs()
-#         s.pop()
-#         # print(s)
-#         # print(visited)
-#         visited[i] = False
-            
-
-# n, m = map(int, input().split())
-# s = []
-# visited = [False] * (n+1)
-
-# dfs()
-        
-    
