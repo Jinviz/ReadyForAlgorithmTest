61fa968399eb07e523eff50e03cd17000f8333dd
JINWOOKIM <rlawlsdn9583@gmail.com>
2024-09-10 11:02:13 +0900

    9012_S4_A

---
+++ BaekJoon/Data Structure/9012_괄호.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(input())
+
+for _ in range(N):
+    input = sys.stdin.readline().rstrip()
+    same = 0
+    for n in range(len(input)):
+        if same < 0:
+            break
+        if input[n] == '(':
+            same += 1
+        else:
+            same -= 1
+    print('YES' if same == 0 else 'NO')
+        
