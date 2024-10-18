26e57ff2243b8e72971d43d056095331b9aab64a
Ray <rlawlsdn9583@gmail.com>
2024-10-19 04:09:52 +0900

    20291_S3_A_1t_https://www.acmicpc.net/status?user_id=kb1812&problem_id=20291&from_mine=1

---
+++ BaekJoon/Implementation/20291_파일정리.py
@@ -0,0 +1,16 @@
+import sys
+N = int(input())
+files = {}
+
+for _ in range(N):
+    file = sys.stdin.readline().rstrip()
+    dot = file.find('.')
+    print(file[dot+1:])
+    if file[dot+1:] in files:
+        files[file[dot+1:]] += 1
+    else:
+        files[file[dot+1:]] = 1
+
+file_count = sorted(files.items())
+for f in file_count:
+    print(' '.join(map(str,f)))
